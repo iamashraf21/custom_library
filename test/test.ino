5c2e33f3cd422ec463394fde75b92eb193e874b4
iamashraf21 <iamashraf21@gmail.com>
2021-07-01 15:57:10 +0530

    adding test.ino

---
+++ test/test.ino
@@ -0,0 +1,31 @@
+#include <AUnit.h>
+
+test(correct) {
+  int x = 1;
+  assertEqual(x, 1);
+}
+
+test(incorrect) {
+  int x = 1;
+  assertNotEqual(x, 1);
+}
+
+test(Custom) {
+  int x = 4;
+  assertEqual(x, 4);
+}
+
+//----------------------------------------------------------------------------
+// setup() and loop()
+//----------------------------------------------------------------------------
+
+void setup() {
+  delay(1000); // wait for stability on some boards to prevent garbage Serial
+  Serial.begin(115200); // ESP8266 default of 74880 not supported on Linux
+  while(!Serial); // for the Arduino Leonardo/Micro only
+}
+
+void loop() {
+  aunit::TestRunner::run();
+  delay(500);
+}
