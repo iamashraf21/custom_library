03e82f5e64c727dfb9feda663a9821463ad3e569
iamashraf21 <iamashraf21@gmail.com>
2021-07-02 15:40:00 +0530

    test script

---
+++ ci/run-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Starting the shell script"
+# Exit immediately if a command exits with a non-zero status.
+set -e
+# Enable the globstar shell option
+shopt -s globstar
+# Make sure we are inside the github workspace
+
+cd $HOME/Documents/myagent/_work/4/s/custom_library/test/
+arduino-cli compile -b Seeeduino:samd:zero -e
+arduino-cli upload -p /dev/ttyACM0 -b Seeeduino:samd:zero
+
+sleep 5
+python SerialRead.py
+
