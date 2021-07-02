8016ab04fc072dd35b15ab55a9c2e2090403ca2c
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-07-02 16:27:14 +0530

    Update run-test.sh

--- ci/run-test.sh
+++ ci/run-test.sh
@@ -4,12 +4,7 @@ echo "Starting the shell script"
 set -e
 # Enable the globstar shell option
 shopt -s globstar
-# Make sure we are inside the github workspace
-
-<<<<<<< HEAD
 ls -lRh
-=======
->>>>>>> 69c383ba5c0a9dda1631449ff6b38ebe3cc06835
 #cd $HOME/Documents/myagent/_work/4/s/custom_library/test/
 #arduino-cli compile -b Seeeduino:samd:zero -e
 #arduino-cli upload -p /dev/ttyACM0 -b Seeeduino:samd:zero
