7cf24de216fe6f6aaa8db47827bdb271da06cb1a
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-07-14 09:11:34 +0530

    Update run-test.sh

--- ci/run-test.sh
+++ ci/run-test.sh
@@ -9,16 +9,14 @@ echo "compiling examples"
 
 #arduino-cli core search Moteino
 
-#arduino-cli core install Moteino:samd
+arduino-cli core install Moteino:samd
 
 arduino-cli lib install micro-ecc
 arduino-cli lib install AUnit
 
-whoami
-
-ls -l /dev/ttyACM0
-usermod -a -G dialout $USER
-chmod a+rw /dev/ttyACM0
+#ls -l /dev/ttyACM0
+#usermod -a -G dialout $USER
+#chmod a+rw /dev/ttyACM0
 #cd ../examples/ecc_test
 #arduino-cli compile -b Moteino:samd:moteino_m0 -e
 echo "compiling Aunit testcases"
