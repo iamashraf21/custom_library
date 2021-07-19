99ad5efbe3f4c68d1df7f76cd27a261d199f274d
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-07-19 10:44:33 +0530

    Update run-test.sh

--- ci/run-test.sh
+++ ci/run-test.sh
@@ -12,7 +12,7 @@ pwd
 
 cd ../examples/ecc_test
 arduino-cli compile -b Moteino:samd:moteino_m0 -e 
-sudo cp -r /__w/15/s/AUnit ~/Arduino/libraries/
+#sudo cp -r /__w/15/s/AUnit ~/Arduino/libraries/
 
 cd ../../test
 
