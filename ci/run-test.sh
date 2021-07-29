42997082dea92bccf49e1195ccf3779f62aa5a1d
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-07-29 14:05:05 +0530

    Update run-test.sh

--- ci/run-test.sh
+++ ci/run-test.sh
@@ -5,15 +5,13 @@ set -e
 # Enable the globstar shell option
 shopt -s globstar
 
-echo "compiling examples"
-cd ../examples/ecc_test
-arduino-cli compile -b Seeeduino:samd:zero -e
 
-echo "compiling Aunit testcases"
-cd ../../test
-arduino-cli compile -b Seeeduino:samd:zero -e
-arduino-cli upload -p /dev/ttyACM0 -b Seeeduino:samd:zero
+cd ../test
 
+arduino-cli compile -b Moteino:samd:moteino_m0 -e
+arduino-cli upload -p /dev/ttyACM0 -b Moteino:samd:moteino_m0
+
+cd ../../AUnit/extras
 sleep 5
 python SerialRead.py
 
